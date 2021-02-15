73119841e204bb3bc48a23e3e0a8b1f139164358
Trai Lynne <trailynne@gmail.com>
2021-02-14 19:18:57 -0700

    Complete Task 2 Return an array of all 'Final' Game data

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+   return data.filter(g => g['Stage'] == 'Final')
 }
 
 
